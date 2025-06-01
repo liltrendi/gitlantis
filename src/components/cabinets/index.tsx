719bf4b53288d9f5fd46819b7ceb960607a500e7
Brian Njogu <briancanspit@gmail.com>
2025-06-01 04:17:22 +0300

    feat: refactor cabinet name into rounder backdrop

--- src/components/cabinets/index.tsx
+++ src/components/cabinets/index.tsx
@@ -1,12 +1,14 @@
-import { Clone, Text } from "@react-three/drei";
+import { Clone } from "@react-three/drei";
 import { useCabinetModel } from "@/hooks/useCabinet";
 import { useCabinetMovement } from "@/hooks/useCabinetMovement";
 import { useCabinetGeneration } from "@/hooks/useCabinetGeneration";
 import { useCabinetCollision } from "@/hooks/useCabinetCollision";
 import { useGameContext } from "@/hooks/useGameContext";
+import { Backdrop } from "../shared/backdrop";
 
 export const Cabinets = () => {
-  const { worldOffsetRef, boatRef, cabinetsRef, projectInfoRef } = useGameContext();
+  const { worldOffsetRef, boatRef, cabinetsRef, projectInfoRef } =
+    useGameContext();
 
   const model = useCabinetModel();
 
@@ -23,12 +25,9 @@ export const Cabinets = () => {
     <>
       {cabinets.map((instance, index) => {
         const text = projectInfoRef?.current?.directories?.[index];
-        const backgroundWidth = text.length * 0.15;
-        const textYRotation = -Math.PI / 2;
-
         return (
           // @ts-expect-error
-          <group key={instance.key}>s
+          <group key={instance.key}>
             <Clone
               ref={(el) => {
                 if (!cabinetsRef?.current) return;
@@ -38,22 +37,7 @@ export const Cabinets = () => {
               object={model}
               scale={60}
             >
-              {/* @ts-expect-error */}
-              <group position={[0, 1.53, 0]}>
-              {/* @ts-expect-error */}
-                <mesh position={[0, 0, -0.01]} rotation-y={textYRotation}><planeGeometry args={[backgroundWidth, 0.5]} /><meshBasicMaterial color="#000" transparent opacity={0.85} /></mesh>
-                <Text
-                  fontSize={0.2}
-                  color="#f2bc07"
-                  anchorX="center"
-                  anchorY="middle"
-                  rotation-y={textYRotation}
-                  position-x={-0.1}
-                >
-                  {text}
-                </Text>
-                {/* @ts-expect-error */}
-              </group>
+              <Backdrop text={text} />
             </Clone>
             {/* @ts-expect-error */}
           </group>
---
+++ src/components/shared/backdrop/index.tsx
@@ -0,0 +1,70 @@
+import { Text } from "@react-three/drei";
+import { useMemo } from "react";
+import { Shape } from "three";
+
+export const Backdrop = ({ text = "" }: { text: string }) => {
+  const width = text.length * 0.13;
+  const height = 0.5;
+  const radius = 0.1;
+
+  const shape = useMemo(() => {
+    const shape = new Shape();
+
+    shape.moveTo(-width / 2 + radius, -height / 2);
+    shape.lineTo(width / 2 - radius, -height / 2);
+    shape.quadraticCurveTo(
+      width / 2,
+      -height / 2,
+      width / 2,
+      -height / 2 + radius
+    );
+    shape.lineTo(width / 2, height / 2 - radius);
+    shape.quadraticCurveTo(
+      width / 2,
+      height / 2,
+      width / 2 - radius,
+      height / 2
+    );
+    shape.lineTo(-width / 2 + radius, height / 2);
+    shape.quadraticCurveTo(
+      -width / 2,
+      height / 2,
+      -width / 2,
+      height / 2 - radius
+    );
+    shape.lineTo(-width / 2, -height / 2 + radius);
+    shape.quadraticCurveTo(
+      -width / 2,
+      -height / 2,
+      -width / 2 + radius,
+      -height / 2
+    );
+
+    return shape;
+  }, [width, height, radius]);
+
+  return (
+    // @ts-expect-error
+    <group position={[0, 1.53, 0]}>
+      {/* @ts-expect-error */}
+      <mesh position={[0, 0, -0.01]} rotation-y={-Math.PI / 2}>
+        {/* @ts-expect-error */}
+        <shapeGeometry args={[shape]} />
+        {/* @ts-expect-error */}
+        <meshBasicMaterial color="#302c29" transparent opacity={0.85} />
+        {/* @ts-expect-error */}
+      </mesh>
+      <Text
+        fontSize={0.2}
+        color="#f2bc07"
+        anchorX="center"
+        anchorY="middle"
+        rotation-y={-Math.PI / 2}
+        position-x={-0.1}
+      >
+        {text}
+      </Text>
+      {/* @ts-expect-error */}
+    </group>
+  );
+};
