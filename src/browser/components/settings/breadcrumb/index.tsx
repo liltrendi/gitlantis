8c44439fac7119d2ccf1c8450e12718622192e25
Brian Njogu <briancanspit@gmail.com>
2025-06-04 09:57:59 +0300

    fix: breadcrumbs styling

--- src/browser/components/settings/breadcrumb/index.tsx
+++ src/browser/components/settings/breadcrumb/index.tsx
@@ -10,23 +10,20 @@ export const Breadcrumb = () => {
   const segments = currentPath.split("/")?.filter(Boolean);
 
   return (
-    <div className="text-md text-gray-500 flex flex-col items-start absolute top-4 left-4 z-50 rounded-t-lg">
-      <span className="bg-[#f2bc07] px-2 py-[1px] text-md text-black rounded-tl-lg rounded-tr-lg border-t border-l border-r border-[#222]">
+    <div className="text-md text-gray-500 flex flex-col items-start absolute top-3 left-3 z-50 rounded-t-lg">
+      <span className="bg-[#f2bc07] px-2 py-[1px] text-md text-black rounded-tl-lg rounded-tr-lg border-t border-l border-r border-[#2d302f] border-l-4">
         File explorer
       </span>
-      <nav className="flex bg-[#222] px-3 py-1 rounded-bl-lg rounded-br-lg rounded-tr-lg">
+      <nav className="flex flex-wrap max-w-[30vw] break-words bg-[#2d302f] px-3 py-1 rounded-bl-lg rounded-br-lg rounded-tr-lg">
         {segments.map((segment, index) => {
           const fullPath = segments.slice(0, index + 1).join("/");
-
           return (
             <span key={fullPath} className="flex items-center">
               {index > 0 && <span className="mx-1 text-gray-400">{">"}</span>}
               <button
                 onMouseDown={(e) => e.preventDefault()}
-                onClick={() => {
-                  setCurrentPath(fullPath);
-                }}
-                className="text-md hover:underline hover:text-[#f2bc07] focus:outline-none"
+                onClick={() => setCurrentPath(fullPath)}
+                className="text-md text-white hover:underline hover:text-[#f2bc07] focus:outline-none"
               >
                 {segment}
               </button>
