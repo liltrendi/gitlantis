03a17d100a4b576f7cdc61f6e10e8a26a84f5cbb
Brian Njogu <briancanspit@gmail.com>
2025-06-11 04:22:03 +0300

    fix: prevent x overflow on mobile settings

--- src/browser/components/settings/content/index.tsx
+++ src/browser/components/settings/content/index.tsx
@@ -9,7 +9,7 @@ export const SettingsContent = (
   gameProps: ReturnType<typeof useGameSettings>
 ) => {
   return (
-    <div className="w-4/5 p-6 overflow-y-auto">
+    <div className="w-4/5 p-6 overflow-y-auto overflow-x-hidden">
       {(() => {
         const label = gameProps.activeTab.label;
         switch (label) {
